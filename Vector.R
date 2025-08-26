1ee07c8014a2cdc9b1f95f9857401e88c4a9489a
Arfan Siregar <arfan.siregar@finansia.com>
2025-08-26 22:51:14 +0700

    save exercise vector

---
+++ Vector.R
@@ -0,0 +1,9 @@
+# Ini adalah contoh penggunaan vector untuk angka numerik
+c(2, 3, 4)
+
+# Variable bernama angka dengan inputan berupa vector
+angka <- c(2, 5, 3)
+
+# Menampilkan isi variable angka dengan print
+print(angka)
+class(angka)
