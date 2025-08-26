d141711406d1ccb720f307a32e640122d45d9e91
Arfan Siregar <arfan.siregar@finansia.com>
2025-08-26 23:07:27 +0700

    save exercise using operator on vector

--- Vector.R
+++ Vector.R
@@ -6,4 +6,19 @@ angka <- c(2, 5, 3)
 
 # Menampilkan isi variable angka dengan print
 print(angka)
+
+# Menampilkan tipe dari angka
 class(angka)
+
+# Variable vector manualNumeric di isi dengan nilai berurut 1 hingga 10 secara manual
+manualNumeric <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+# Menampilkan isi dari manualNumeric yang berurut dari 1 hingga 10
+print(manualNumeric)
+
+# Variable vector operatorNumeric di isi dengan otomatis menggunakan :
+operatorNumeric <- c(1:10)
+
+# Menampilkan isi dari operatorNumeric yang berurut dari 1 hingga 10
+print(operatorNumeric)
+
